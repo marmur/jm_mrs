18ddbe41359a291d6459e086ba30a672eefc4afc
Mateusz Mrowka <mmrowek@gamil.com>
2011-02-24 22:26:28 +0100

    Map interface added

---
+++ MTest/MTest/map/IMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTest.map
+{
+    public interface MapAware
+    {
+        void SetMap(IMap map);
+    }
+
+    public interface IMap
+    {
+
+    }
+
+    public interface IMapManagment: IMap
+    {
+        void StartWork();
+        void StopWork();
+    }
+
+    public interface IMapFactory
+    {
+        IMapManagment CreateMap(Vector startPosition);
+    }
+}
---
+++ MTest/MTest/map/Map.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MTest.map
+{
+    public class Map : IMapManagment
+    {
+        private bool _finish;
+
+        private bool Finish
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _finish;
+                }
+
+            }
+
+            set
+            {
+                lock (this)
+                {
+                    _finish = value;
+                }
+            }
+        }
+
+        Thread mapThread;
+
+        public void StartWork()
+        {
+            if (mapThread == null)
+            {
+                mapThread = new Thread(new ThreadStart(this.DoWork));
+                mapThread.Name = "mapThread";
+                mapThread.Start();
+            }
+        }
+
+        public void StopWork()
+        {
+            if (mapThread != null)
+            {
+                Finish = true;
+                if (!mapThread.Join(2000))
+                {
+                    mapThread.Abort();
+                    mapThread.Join();
+                }
+                mapThread = null;
+            }
+        }
+
+
+        private void DoWork()
+        {
+            while (!Finish)
+            {
+                Thread.Sleep(2000);
+            }
+        }
+    }
+
+    public class MapFactory : IMapFactory
+    {
+        #region IMapFactory Members
+
+        public IMapManagment CreateMap(Vector startPosition)
+        {
+            return new Map();
+        }
+
+        #endregion
+    }
+
+
+}
